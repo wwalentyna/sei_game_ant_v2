c714fbb8cb88b41339cff0422d6d93b2dd98f3cf
cheichan1-hash <cheichan1@gmail.com>
2026-06-08 17:55:18 +0200

    change

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // Relative path is safer for GitHub Pages, especially if the repository name changes.
-  base: './',
-});
